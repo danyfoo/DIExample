53c160b7abf5418a49848160f711237d83e060d2
Daniel Rivera Guzman <darivera@fender.com>
2016-04-01 11:33:34 -0700

    Se modifico la clase RentABike para que fuera una interfaz que ayudara a atender los servicios de DI

--- src/diexample/RentABike.java
+++ src/diexample/RentABike.java
@@ -5,41 +5,13 @@
  */
 package diexample;
 
-import direalexample.*;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
  *
  * @author darivera
  */
-public class RentABike {
-    private String storeName;
-    final List bikes = new ArrayList();
-
-    public RentABike(String storeName) {
-        this.storeName = storeName;
-        
-        bikes.add(new Bike("Shimano", "Roadmaster", 20, "11111", 15, "Fair"));
-        
-        bikes.add(new Bike("Cannondale", "F20000 XTR", 18, "22222", 15, "Excellent"));
-        
-        bikes.add(new Bike("Trek", "6000", 19, "33333", 12.4, "Fair"));
-    }
-    
-    @Override
-    public String toString(){ return "RentABike: " + storeName; }
-
-    public List getBikes() { return bikes; }
-    
-    public Bike getBike(String serialNo) {
-        Iterator iter = bikes.iterator();
-        
-        while (iter.hasNext()) {            
-            Bike bike = (Bike) iter.next();
-            if(serialNo.equals(bike.getSerialNo())) return bike;
-        }
-        return null;
-    }
+interface RentABike {
+    List getBike();
+    Bike getBike(String serialNo);
 }
